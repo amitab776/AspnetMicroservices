300ba1fcb463773d57b0c7fdb499f1b488166231
Abhash Amit <amitab776@gmail.com>
2021-10-16 23:52:33 +0530

    Finished the application layer and continiued with the Infrastructure part.

---
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,65 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.DeleteOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediatr;
+
+        public OrderController(IMediator mediatr)
+        {
+            _mediatr = mediatr ?? throw new ArgumentNullException(nameof(mediatr));
+        }
+
+        [HttpGet("{userName}", Name ="GetOrder")]
+        [ProducesResponseType(typeof(IEnumerable<OrderVm>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<OrderVm>>> GetOrdersByUserName(string userName)
+        {
+            var query = new GetOrderListQuery(userName);
+            var orders = await _mediatr.Send(query).ConfigureAwait(false);
+
+            return Ok(orders);
+        }
+
+        [HttpPost(Name = "CheckoutOrder")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<int>> CheckoutOrder([FromBody] CheckoutOrderCommand command)
+        {
+            await _mediatr.Send(command).ConfigureAwait(false);
+            return NoContent();
+        }
+
+        [HttpPut(Name ="UpdateOrder")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> UpdateOrder([FromBody] UpdateOrderCommand command)
+        {
+            await _mediatr.Send(command).ConfigureAwait(false);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}", Name ="DeleteOrder")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult> DeleteOrder(int id)
+        {
+            var query = new DeleteOrderCommand() { Id = id };
+            await _mediatr.Send(query).ConfigureAwait(false);
+
+            return NoContent();
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreConfiguredOrders());
+                await orderContext.SaveChangesAsync().ConfigureAwait(false);
+
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext));
+            }
+        }
+
+        public static IEnumerable<Order> GetPreConfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order
+                {
+                    UserName = "swn",
+                    FirstName = "Abhash",
+                    LastName = "ab",
+                    EmailAddress = "amitab776@gmail.com",
+                    TotalPrice = 1500
+                }
+            };
+        }
+    }
+}
